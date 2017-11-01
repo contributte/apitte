ea56762ecc09268d99fa15bce1fb787282b038f6
Milan Felix Šulc <rkfelix@gmail.com>
2017-11-01 19:53:05 +0100

    Schema: update serialization

--- src/Schema/Serialization/ArrayHydrator.php
+++ src/Schema/Serialization/ArrayHydrator.php
@@ -8,7 +8,6 @@ use Apitte\Core\Schema\EndpointHandler;
 use Apitte\Core\Schema\EndpointNegotiation;
 use Apitte\Core\Schema\EndpointParameter;
 use Apitte\Core\Schema\Schema;
-use Apitte\Core\Schema\SchemaMapping;
 
 final class ArrayHydrator implements IHydrator
 {
@@ -21,61 +20,75 @@ final class ArrayHydrator implements IHydrator
 	{
 		$schema = new Schema();
 
-		foreach ($data as $route) {
-
-			if (!isset($route[SchemaMapping::HANDLER])) {
-				throw new InvalidStateException(sprintf('Structure %s is required', SchemaMapping::HANDLER));
-			}
-
-			$handler = new EndpointHandler();
-			$handler->setClass($route[SchemaMapping::HANDLER][SchemaMapping::HANDLER_CLASS]);
-			$handler->setMethod($route[SchemaMapping::HANDLER][SchemaMapping::HANDLER_METHOD]);
-			$handler->setArguments($route[SchemaMapping::HANDLER][SchemaMapping::HANDLER_ARGUMENTS]);
-
-			$endpoint = new Endpoint();
-			$endpoint->setHandler($handler);
-			$endpoint->setMethods($route[SchemaMapping::METHODS]);
-			$endpoint->setMask($route[SchemaMapping::MASK]);
-			$endpoint->setAttribute(SchemaMapping::RAW_PATTERN, $route[SchemaMapping::RAW_PATTERN]);
-
-			if (isset($route[SchemaMapping::GROUP])) {
-				if (isset($route[SchemaMapping::GROUP][SchemaMapping::GROUP_IDS])) {
-					$endpoint->addTag(Endpoint::TAG_GROUP_IDS, (array) $route[SchemaMapping::GROUP][SchemaMapping::GROUP_IDS]);
-				}
-				if (isset($route[SchemaMapping::GROUP][SchemaMapping::GROUP_PATHS])) {
-					$endpoint->addTag(Endpoint::TAG_GROUP_PATHS, (array) $route[SchemaMapping::GROUP][SchemaMapping::GROUP_PATHS]);
-				}
-			}
-
-			if (isset($route[SchemaMapping::ID])) {
-				$endpoint->addTag(Endpoint::TAG_ID, $route[SchemaMapping::ID]);
-			}
-
-			if (isset($route[SchemaMapping::TAGS])) {
-				foreach ($route[SchemaMapping::TAGS] as $name => $value) {
-					$endpoint->addTag($name, $value);
-				}
-			}
-
-			foreach ($route[SchemaMapping::PARAMETERS] as $param) {
-				$parameter = new EndpointParameter();
-				$parameter->setName($param[SchemaMapping::PARAMETERS_NAME]);
-				$parameter->setType($param[SchemaMapping::PARAMETERS_TYPE]);
-				$parameter->setDescription($param[SchemaMapping::PARAMETERS_DESCRIPTION]);
-				$endpoint->addParameter($parameter);
-			}
-
-			foreach ($route[SchemaMapping::NEGOTIATIONS] as $nego) {
-				$negotiation = new EndpointNegotiation();
-				$negotiation->setType($nego[SchemaMapping::NEGOTIATIONS_TYPE]);
-				$negotiation->setMetadata($nego[SchemaMapping::NEGOTIATIONS_METADATA]);
-				$endpoint->addNegotiation($negotiation);
-			}
-
+		foreach ($data as $endpoint) {
+			$endpoint = $this->hydrateEndpoint($endpoint);
 			$schema->addEndpoint($endpoint);
 		}
 
 		return $schema;
+	}
+
+	/**
+	 * @param array $data
+	 * @return Endpoint
+	 */
+	protected function hydrateEndpoint(array $data)
+	{
+		if (!isset($data['handler'])) {
+			throw new InvalidStateException("Schema route 'handler' is required");
+		}
+
+		$endpoint = new Endpoint();
+		$endpoint->setMethods($data['methods']);
+		$endpoint->setMask($data['mask']);
+
+		if (isset($data['description'])) {
+			$endpoint->setDescription($data['description']);
+		}
+
+		$handler = new EndpointHandler();
+		$handler->setClass($data['handler']['class']);
+		$handler->setMethod($data['handler']['method']);
+		$handler->setArguments($data['handler']['arguments']);
+		$endpoint->setHandler($handler);
+
+		if (isset($data['id'])) {
+			$endpoint->addTag(Endpoint::TAG_ID, $data['id']);
+		}
+
+		if (isset($data['tags'])) {
+			foreach ($data['tags'] as $name => $value) {
+				$endpoint->addTag($name, $value);
+			}
+		}
+
+		if (isset($data['attributes'])) {
+			if (isset($data['attributes']['pattern'])) {
+				$endpoint->setAttribute('pattern', $data['attributes']['pattern']);
+			}
+		}
+
+		if (isset($data['parameters'])) {
+			foreach ($data['parameters'] as $param) {
+				$parameter = new EndpointParameter();
+				$parameter->setName($param['name']);
+				$parameter->setType($param['type']);
+				$parameter->setDescription($param['description']);
+				$endpoint->addParameter($parameter);
+			}
+		}
+
+		if (isset($data['negotiations'])) {
+			foreach ($data['negotiations'] as $nego) {
+				$negotiation = new EndpointNegotiation();
+				$negotiation->setSuffix($nego['suffix']);
+				$negotiation->setDefault($nego['default']);
+				$negotiation->setCallback($nego['callback']);
+				$endpoint->addNegotiation($negotiation);
+			}
+		}
+
+		return $endpoint;
 	}
 
 }
--- src/Schema/Serialization/ArraySerializator.php
+++ src/Schema/Serialization/ArraySerializator.php
@@ -2,9 +2,10 @@
 
 namespace Apitte\Core\Schema\Serialization;
 
+use Apitte\Core\Schema\Builder\Controller\Controller;
+use Apitte\Core\Schema\Builder\Controller\Method;
 use Apitte\Core\Schema\Builder\SchemaBuilder;
 use Apitte\Core\Schema\EndpointParameter;
-use Apitte\Core\Schema\SchemaMapping;
 use Apitte\Core\Utils\Helpers;
 use Apitte\Core\Utils\Regex;
 
@@ -29,97 +30,107 @@ final class ArraySerializator implements ISerializator
 				// Skip invalid methods
 				if (empty($method->getPath())) continue;
 
-				// Build full mask (@GroupPath(s) + @ControllerPath + @Path)
-				// without duplicated slashes (//)
-				// and without trailing slash at the end
-				// but with slash at the beginning
-				$maskp = array_merge(
-					$controller->getGroupPaths(),
-					[$controller->getPath()],
-					[$method->getPath()]
-				);
-				$mask = implode('/', $maskp);
-				$mask = Helpers::slashless($mask);
-				$mask = '/' . trim($mask, '/');
-
-				// Build full id (@GroupId(s) + @ControllerId + @Id)
-				// If @Id is empty, then fullid is also empty
-				if (empty($method->getId())) {
-					$id = NULL;
-				} else {
-					$idp = array_merge(
-						$controller->getGroupIds(),
-						[$controller->getId()],
-						[$method->getId()]
-					);
-					$id = implode('.', $idp);
-				}
-
-				// Create endpoint
-				$endpoint = [
-					SchemaMapping::HANDLER => [
-						SchemaMapping::HANDLER_CLASS => $controller->getClass(),
-						SchemaMapping::HANDLER_METHOD => $method->getName(),
-						SchemaMapping::HANDLER_ARGUMENTS => $method->getArguments(),
-					],
-					SchemaMapping::GROUP => [
-						SchemaMapping::GROUP_IDS => $controller->getGroupIds(),
-						SchemaMapping::GROUP_PATHS => $controller->getGroupPaths(),
-					],
-					SchemaMapping::ID => $id,
-					SchemaMapping::TAGS => $controller->getTags(),
-					SchemaMapping::METHODS => $method->getMethods(),
-					SchemaMapping::MASK => $mask,
-					SchemaMapping::PARAMETERS => [],
-					SchemaMapping::RAW_PATTERN => $mask,
-				];
-
-				// Collect variable parameters from URL
-				$pattern = Regex::replaceCallback($mask, '#({([a-zA-Z0-9\-_]+)})#U', function ($matches) use (&$endpoint, $method) {
-					list($whole, $variable, $variableName) = $matches;
-
-					// Build parameter pattern
-					$pattern = sprintf('(?P<%s>[^/]+)', $variableName);
-
-					// Build parameters
-					$parameters = [
-						SchemaMapping::PARAMETERS_NAME => $variableName,
-						SchemaMapping::PARAMETERS_PATTERN => $pattern,
-						SchemaMapping::PARAMETERS_TYPE => EndpointParameter::TYPE_SCALAR,
-						SchemaMapping::PARAMETERS_DESCRIPTION => NULL,
-					];
-
-					// Update endpoint parameters by defined annotation
-					if ($method->hasParameter($variableName)) {
-						$parameter = $method->getParameters()[$variableName];
-						$parameters[SchemaMapping::PARAMETERS_TYPE] = $parameter->getType();
-						$parameters[SchemaMapping::PARAMETERS_DESCRIPTION] = $parameter->getDescription();
-					}
-
-					// Update endpoint
-					$endpoint[SchemaMapping::PARAMETERS][$variableName] = $parameters;
-
-					// Returned pattern replace {variable} in mask
-					return $pattern;
-				});
-
-				$endpoint[SchemaMapping::NEGOTIATIONS] = [];
-				foreach ($method->getNegotiations() as $negotiation) {
-					$endpoint[SchemaMapping::NEGOTIATIONS][] = [
-						SchemaMapping::NEGOTIATIONS_TYPE => $negotiation->getType(),
-						SchemaMapping::NEGOTIATIONS_METADATA => $negotiation->getMetadata(),
-					];
-				}
-
-				// Build final regex pattern
-				$endpoint[SchemaMapping::RAW_PATTERN] = $pattern;
-
-				// Append to schema
+				$endpoint = $this->serializeEndpoint($controller, $method);
 				$schema[] = $endpoint;
 			}
 		}
 
 		return $schema;
+	}
+
+	/**
+	 * @param Controller $controller
+	 * @param Method $method
+	 * @return array
+	 */
+	protected function serializeEndpoint(Controller $controller, Method $method)
+	{
+		// Build full mask (@GroupPath(s) + @ControllerPath + @Path)
+		// without duplicated slashes (//)
+		// and without trailing slash at the end
+		// but with slash at the beginning
+		$maskp = array_merge(
+			$controller->getGroupPaths(),
+			[$controller->getPath()],
+			[$method->getPath()]
+		);
+		$mask = implode('/', $maskp);
+		$mask = Helpers::slashless($mask);
+		$mask = '/' . trim($mask, '/');
+
+		// Build full id (@GroupId(s) + @ControllerId + @Id)
+		// If @Id is empty, then fullid is also empty
+		if (empty($method->getId())) {
+			$id = NULL;
+		} else {
+			$idp = array_merge(
+				$controller->getGroupIds(),
+				[$controller->getId()],
+				[$method->getId()]
+			);
+			$id = implode('.', $idp);
+		}
+
+		// Create endpoint
+		$endpoint = [
+			'handler' => [
+				'class' => $controller->getClass(),
+				'method' => $method->getName(),
+				'arguments' => $method->getArguments(),
+			],
+			'id' => $id,
+			'tags' => $controller->getTags(),
+			'methods' => $method->getMethods(),
+			'mask' => $mask,
+			'description' => $method->getDescription(),
+			'parameters' => [],
+			'negotiations' => [],
+			'attributes' => [
+				'pattern' => NULL,
+			],
+		];
+
+		// Collect variable parameters from URL
+		$pattern = Regex::replaceCallback($mask, '#({([a-zA-Z0-9\-_]+)})#U', function ($matches) use (&$endpoint, $method) {
+			list($whole, $variable, $variableName) = $matches;
+
+			// Build parameters
+			$parameter = [
+				'name' => $variableName,
+				'type' => EndpointParameter::TYPE_SCALAR,
+				'description' => NULL,
+			];
+
+			// Update endpoint parameters by defined annotation
+			if ($method->hasParameter($variableName)) {
+				$param = $method->getParameters()[$variableName];
+				$parameter['type'] = $param->getType();
+				$parameter['description'] = $param->getDescription();
+			}
+
+			// Build parameter pattern
+			$pattern = sprintf('(?P<%s>[^/]+)', $variableName);
+			$parameter['pattern'] = $pattern;
+
+			// Update endpoint
+			$endpoint['parameters'][$variableName] = $parameter;
+
+			// Returned pattern replace {variable} in mask
+			return $pattern;
+		});
+
+		foreach ($method->getNegotiations() as $negotiation) {
+			$endpoint['negotiations'][] = [
+				'suffix' => $negotiation->getSuffix(),
+				'default' => $negotiation->isDefault(),
+				'callback' => $negotiation->getCallback(),
+			];
+		}
+
+		// Build final regex pattern
+		$endpoint['attributes']['pattern'] = $pattern;
+
+		return $endpoint;
 	}
 
 }

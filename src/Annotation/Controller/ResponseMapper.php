6523e4a7fbcb5be2c71329bafdefc6134949f614
Marek Bartos <bartos.developer152@gmail.com>
2018-08-21 00:07:22 +0200

    Check entity

--- src/Annotation/Controller/ResponseMapper.php
+++ src/Annotation/Controller/ResponseMapper.php
@@ -2,6 +2,7 @@
 
 namespace Apitte\Core\Annotation\Controller;
 
+use Apitte\Core\Mapping\Response\IResponseEntity;
 use Doctrine\Common\Annotations\Annotation\Target;
 use Doctrine\Common\Annotations\AnnotationException;
 
@@ -20,8 +21,16 @@ final class ResponseMapper
 	 */
 	public function __construct(array $values)
 	{
-		if (!isset($values['entity'])) {
+		if (!isset($values['entity']) || empty($values['entity'])) {
 			throw new AnnotationException('Empty @ResponseMapper entity given');
+		}
+
+		if (!class_exists($values['entity'])) {
+			throw new AnnotationException(sprintf('@ResponseMapper entity "%s" does not exists', $values['entity']));
+		}
+
+		if (!isset(class_implements($values['entity'])[IResponseEntity::class])) {
+			throw new AnnotationException(sprintf('@ResponseMapper entity "%s" does not implements "%s"', $values['entity'], IResponseEntity::class));
 		}
 
 		$this->entity = $values['entity'];

e44d8cbe143ee03cd9d619a537e3dbc941f9b614
Marek Bartos <bartos.developer152@gmail.com>
2018-10-02 13:57:56 +0200

    Limit allowed codes for api exceptions

--- src/Exception/Api/ClientErrorException.php
+++ src/Exception/Api/ClientErrorException.php
@@ -3,6 +3,7 @@
 namespace Apitte\Core\Exception\Api;
 
 use Apitte\Core\Exception\ApiException;
+use Apitte\Core\Exception\Logical\InvalidArgumentException;
 use Throwable;
 
 /**
@@ -16,6 +17,10 @@ class ClientErrorException extends ApiException
 	 */
 	public function __construct(string $message = '', int $code = 400, ?Throwable $previous = null, $context = null)
 	{
+		if ($code < 400 || $code > 499) {
+			throw new InvalidArgumentException(sprintf('%s code could be only in range from 400 to 499', static::class));
+		}
+
 		parent::__construct($message, $code, $previous, $context);
 	}
 

3d8c8b43f118db2914468a483dfd84a0dc0a1d1f
Marek Bartos <bartos.developer152@gmail.com>
2019-02-08 12:34:54 +0100

    Simplify handlers

--- src/ErrorHandler/TracyErrorHandler.php
+++ src/ErrorHandler/TracyErrorHandler.php
@@ -2,15 +2,11 @@
 
 namespace Apitte\Core\ErrorHandler;
 
-use Apitte\Core\Exception\Runtime\SnapshotException;
 use Throwable;
 use Tracy\ILogger;
 
-class TracyErrorHandler implements IErrorHandler
+class TracyErrorHandler extends SimpleErrorHandler
 {
-
-	/** @var bool */
-	private $catchException = false;
 
 	/** @var ILogger */
 	private $logger;
@@ -20,25 +16,11 @@ class TracyErrorHandler implements IErrorHandler
 		$this->logger = $logger;
 	}
 
-	public function setCatchException(bool $catchException): void
-	{
-		$this->catchException = $catchException;
-	}
-
 	public function handle(Throwable $throwable): void
 	{
 		$this->logger->log($throwable, ILogger::EXCEPTION);
 
-		// Rethrow error if it should not be catch (debug only)
-		if (!$this->catchException) {
-
-			// Unwrap exception from snapshot
-			if ($throwable instanceof SnapshotException) {
-				throw $throwable->getPrevious();
-			}
-
-			throw $throwable;
-		}
+		parent::handle($throwable);
 	}
 
 }

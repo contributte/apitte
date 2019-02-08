0e936ae7ea94c8e8e4690152e3f17d27d8a9d980
Marek Bartos <bartos.developer152@gmail.com>
2019-02-08 12:34:54 +0100

    Simplify handlers

--- src/ErrorHandler/PsrErrorHandler.php
+++ src/ErrorHandler/PsrErrorHandler.php
@@ -2,45 +2,26 @@
 
 namespace Apitte\Core\ErrorHandler;
 
-use Apitte\Core\Exception\Runtime\SnapshotException;
 use Apitte\Core\Utils\Helpers;
 use Psr\Log\LoggerInterface;
 use Throwable;
 
-class PsrErrorHandler implements IErrorHandler
+class PsrErrorHandler extends SimpleErrorHandler
 {
 
 	/** @var LoggerInterface */
 	private $logger;
-
-	/** @var bool */
-	private $catchException = false;
 
 	public function __construct(LoggerInterface $logger)
 	{
 		$this->logger = $logger;
 	}
 
-	public function setCatchException(bool $catchException): void
-	{
-		$this->catchException = $catchException;
-	}
-
 	public function handle(Throwable $throwable): void
 	{
-		// Log exception
 		$this->logger->error($throwable->getMessage(), ['exception' => Helpers::throwableToArray($throwable)]);
 
-		// Rethrow error if it should not be catch (debug only)
-		if (!$this->catchException) {
-
-			// Unwrap exception from snapshot
-			if ($throwable instanceof SnapshotException) {
-				throw $throwable->getPrevious();
-			}
-
-			throw $throwable;
-		}
+		parent::handle($throwable);
 	}
 
 }

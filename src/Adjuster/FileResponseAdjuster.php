88de79a23493a126cb017d9ef809794b48b34637
Marek Bartos <bartos.developer152@gmail.com>
2019-02-20 19:38:26 +0100

    Make FileResponseAdjuster static

--- src/Adjuster/FileResponseAdjuster.php
+++ src/Adjuster/FileResponseAdjuster.php
@@ -8,7 +8,7 @@ use Psr\Http\Message\StreamInterface;
 class FileResponseAdjuster
 {
 
-	public function adjust(
+	public static function adjust(
 		ResponseInterface $response,
 		StreamInterface $stream,
 		string $filename,

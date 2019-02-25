e48f2eea0931e7c0b217fda72b1f0c0313e11d00
Marek Bartos <bartos.developer152@gmail.com>
2019-02-25 03:20:19 +0100

    Run application with given request

--- src/Application/IApplication.php
+++ src/Application/IApplication.php
@@ -2,9 +2,13 @@
 
 namespace Apitte\Core\Application;
 
+use Psr\Http\Message\ServerRequestInterface;
+
 interface IApplication
 {
 
 	public function run(): void;
+
+	public function runWith(ServerRequestInterface $request): void;
 
 }

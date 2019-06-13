ff6ed9f4201f8b5c25a9eaafb0f4d13f7d508b1c
Marek Bartoš <bartos.developer152@gmail.com>
2019-06-13 14:07:46 +0200

    DI: Drop Plugin interface

--- src/DI/Plugin/AbstractPlugin.php
+++ src/DI/Plugin/AbstractPlugin.php
@@ -12,7 +12,7 @@ use Nette\Schema\Schema;
 use Nette\Schema\ValidationException;
 use stdClass;
 
-abstract class AbstractPlugin implements Plugin
+abstract class AbstractPlugin
 {
 
 	/** @var PluginCompiler */

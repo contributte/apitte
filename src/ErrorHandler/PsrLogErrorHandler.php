d6d0a6b5e6e8dfd13965247c0d87515652bf7e46
Marek Bartoš <bartos.developer152@gmail.com>
2019-09-21 00:50:08 +0200

    PsrLogErrorHandler: use in logs message of original exception instead of ApiException

--- src/ErrorHandler/PsrLogErrorHandler.php
+++ src/ErrorHandler/PsrLogErrorHandler.php
@@ -32,7 +32,7 @@ class PsrLogErrorHandler extends SimpleErrorHandler
 		if ($error instanceof ApiException && ($previous = $error->getPrevious()) !== null) {
 			// Server error is expected to contain a real error while client error can contain just information, why client request failed
 			$level = $error instanceof ServerErrorException ? LogLevel::ERROR : LogLevel::DEBUG;
-			$this->logger->log($level, $error->getMessage(), ['exception' => $previous]);
+			$this->logger->log($level, $previous->getMessage(), ['exception' => $previous]);
 		}
 
 		return parent::handle($error);

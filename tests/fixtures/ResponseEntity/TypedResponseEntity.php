e9ddb1a7c29b31544bc49a10a4690422d53e2865
Robert Štětka <robert.stetka@gmail.com>
2022-01-21 14:29:45 +0100

    Fix support of typed properties

---
+++ tests/fixtures/ResponseEntity/TypedResponseEntity.php
@@ -0,0 +1,26 @@
+<?php declare(strict_types = 1);
+
+namespace Tests\Fixtures\ResponseEntity;
+
+use DateTime;
+
+class TypedResponseEntity
+{
+
+	public int $int;
+
+	public ?float $nullableFloat;
+
+	public string $string;
+
+	public bool $bool;
+
+	public DateTime $datetime;
+
+	/** @var int */
+	public $phpdocInt;
+
+	// phpcs:ignore
+	public $untypedProperty;
+
+}

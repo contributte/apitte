e3c4087f4564ae0b4bdd90ad3aa9cd41760a1abe
Rastislav Brandobur <rastislav.brandobur@gmail.com>
2024-12-14 11:38:32 +0100

    fix: fixed StringTypeMapper for non scalar values

--- src/Core/Mapping/Parameter/StringTypeMapper.php
+++ src/Core/Mapping/Parameter/StringTypeMapper.php
@@ -2,11 +2,17 @@
 
 namespace Apitte\Core\Mapping\Parameter;
 
+use Apitte\Core\Exception\Runtime\InvalidArgumentTypeException;
+
 class StringTypeMapper implements ITypeMapper
 {
 
 	public function normalize(mixed $value): ?string
 	{
+		if (!is_scalar($value)) {
+			throw new InvalidArgumentTypeException('string');
+		}
+
 		return (string) $value;
 	}
 
